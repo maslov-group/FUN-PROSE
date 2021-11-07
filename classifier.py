31e6f871af0e51f2a7fb6cdb200bd2921ac60e02
Simon Liu <simonl2@illinois.edu>
2021-11-07 01:58:27 -0600

    Modify classifier to use ray for hyperparameter tuning

--- classifier.py
+++ classifier.py
@@ -1,19 +1,22 @@
-import time
+from copy import deepcopy
+import os
+import pandas as pd
+import ray
+from ray import tune
+from ray.tune.suggest.skopt import SkOptSearch
+from scipy.stats import pearsonr
 import torch
-import torch.nn.functional as F
-from torch import optim
 from torch.utils.data import Dataset, DataLoader
+
 from data import JsonDataset, get_samplers
 from data import domain, split_data
-from torch.utils.data.sampler import SubsetRandomSampler
-from copy import deepcopy
-import pandas as pd
 
-if False:
+### Device set-up
+if True:
     print("using gpu")
     cuda = torch.device('cuda')
-    FloatTensor = torch.FloatTensor
-    LongTensor = torch.LongTensor
+    FloatTensor = torch.cuda.FloatTensor
+    LongTensor = torch.cuda.LongTensor
     def cudaify(model):
         return model.cuda()
 else: 
@@ -23,8 +26,7 @@ else:
     LongTensor = torch.LongTensor
     def cudaify(model):
         return model
-    
-UNITS_PER_SIZE = 50
+
 
 class Vocab:
     """
@@ -72,10 +74,10 @@ class Tensorize:
         #print(torch.stack(data['tfs']).shape)
         tfs = torch.stack(data['tfs'],dim=1).float()
         #print(type(data['exp']))
-        category = LongTensor([self.category_vocab(c) 
-                             for c in data['exp']])
-        
-        return cudaify(words), cudaify(tfs), cudaify(category)
+        #label = LongTensor([self.category_vocab(c) 
+        #                     for c in data['exp']])
+        label = data['exp'].float()
+        return cudaify(words), cudaify(tfs), cudaify(label)
         
     @staticmethod
     def words_to_tensor(words, vocab, max_word_length):
@@ -92,7 +94,7 @@ class Tensorize:
         """
         tensor = torch.zeros(len(words), len(vocab), max_word_length)
         for i, word in enumerate(words):
-            for li, letter in enumerate(word):
+            for li, letter in enumerate(word[len(word) - max_word_length:]):
                 tensor[i][vocab(letter)][li] = 1
         return tensor
 
@@ -103,205 +105,241 @@ class ProCNN(torch.nn.Module):
     then trains a fully connected neural network on the to make predictions.
     
     """    
-    def __init__(self, n_input_symbols, hidden_size,
-                 input_symbol_vocab, output_classes):
+    def __init__(self, config, output_classes, input_symbol_vocab):
         super(ProCNN, self).__init__()
         
         self.input_symbol_vocab = input_symbol_vocab
         
-        self.hidden_size=hidden_size
-        
+        self.xt_hidden = config["xt_hidden"]
+        self.xs_hidden = config["seq_length"] // (20 * 10)
+
         self.output_classes = output_classes
         
-        self.conv1 = torch.nn.Conv1d(n_input_symbols,128, kernel_size=5, stride=1, padding=2)
+        self.conv1 = torch.nn.Conv1d(len(input_symbol_vocab),128, kernel_size=15, stride=1, padding=15//2)
         self.pool1 = torch.nn.MaxPool1d(kernel_size=20, stride=20)
-        self.conv2 = torch.nn.Conv1d(128,32, kernel_size=9, stride=1, padding=4)
+        self.activation1 = self.get_activation(config["activation"])
+        
+        self.conv2 = torch.nn.Conv1d(128,32, kernel_size=9, stride=1, padding=9//2)
         self.pool2 = torch.nn.MaxPool1d(kernel_size=10, stride=10)
+        self.activation2 = self.get_activation(config["activation"])
         
-        self.fc_t = torch.nn.Linear(325,hidden_size) #assuming 325 TFs
+        self.fc_t = torch.nn.Linear(325,self.xt_hidden) #assuming 325 TFs
+        self.fc_t_activation = self.get_activation(config["activation"])
         
-        self.fc1 = torch.nn.Linear(32*5+hidden_size, 512)
+        self.fc1 = torch.nn.Linear(32*self.xs_hidden+self.xt_hidden, 512)
         self.bn1 = torch.nn.BatchNorm1d(num_features=512)
+        self.fc_activation1 = self.get_activation(config["activation"])
+        
         self.fc2 = torch.nn.Linear(512, 256)
+        self.bn2 = torch.nn.BatchNorm1d(num_features=256)
+        self.fc_activation2 = self.get_activation(config["activation"])
+        
         self.fc3 = torch.nn.Linear(256, 64)
-        self.fc_out = torch.nn.Linear(64,output_classes)
+        self.bn3 = torch.nn.BatchNorm1d(num_features=64)
+        self.fc_activation3 = self.get_activation(config["activation"])
+        
+        self.fc_out = torch.nn.Linear(64,1)
+        
+    @classmethod
+    def get_activation(cls, activation):
+        if activation == "relu":
+            return torch.nn.ReLU()
+        elif activation == "gelu":
+            return torch.nn.GELU()
+        elif activation == "elu":
+            return torch.nn.ELU()
+        else:
+            raise ValueError("Invalid activation function: must be relu, gelu, or elu.")
     
     def get_input_vocab(self):
         return self.input_symbol_vocab
-    
-    def get_hidden(self, x):
-        pass
     
     def forward(self, x_s, x_t):
         b=list(x_s.size())[0] #batch size
         x_s = self.conv1(x_s)
         x_s = self.pool1(x_s)
-        x_s = F.relu(x_s)
+        x_s = self.activation1(x_s)
+        
         x_s = self.conv2(x_s)
         x_s = self.pool2(x_s)
-        x_s = F.relu(x_s)
+        x_s = self.activation2(x_s)
         
-        x_t = F.relu(self.fc_t(x_t))
+        x_t = self.fc_t(x_t)
+        x_t = self.fc_t_activation(x_t)
         
-        x = torch.cat((x_s.reshape((b,32*5,1)).view(-1, 32*5),x_t),1)
-        x = F.relu(self.fc1(x))
+        x = torch.cat((x_s.reshape((b,32*self.xs_hidden,1)).view(-1, 32*self.xs_hidden),x_t),1)
+        x = self.fc1(x)
         x = self.bn1(x)
-        x = F.relu(self.fc2(x))
-        x = F.relu(self.fc3(x))
+        x = self.fc_activation1(x)
+        
+        x = self.fc2(x)
+        x = self.bn2(x)
+        x = self.fc_activation2(x)
+        
+        x = self.fc3(x)
+        x = self.bn3(x)
+        x = self.fc_activation3(x)
+        
         x = self.fc_out(x)
         #print(self.convs[0].weight.data)
         #yay!
-        return x
-
-class CnnClassifier:
-    """
-    Wraps a trained neural network to use for classification.
+        return x    
     
-    """    
-    def __init__(self, cnn, tensorize):
-        self.net = cnn
-        self.tensorize = tensorize
-     
-    def run(self, data_loader):
-        for data in data_loader:
-            inputs, _ = self.tensorize(data)
-            yield self.net(inputs)  
+class Trainable(tune.Trainable):
+    
+    def setup(self, config, output_classes, char_vocab, category_vocab, trainset, devset):
+        """
+        Initializes a trainer to train a neural network using the provided DataLoaders 
+        (for training and development data).
+
+        tensorize is a function that maps a piece of data (i.e. a dictionary)
+        to an input and output tensor for the neural network.
+        
+        char_vocab is the Vocab object of the input data.
+
+        """
+        print("Setting up Trainable")
+        self.config = config
+        self.tensorize = Tensorize(char_vocab, config["seq_length"], category_vocab)
+        self.char_vocab = char_vocab
+        self.train_loader = DataLoader(trainset, batch_size=config["batch_size"], num_workers=8)
+        self.dev_loader = DataLoader(devset, batch_size=config["batch_size"], num_workers=8)
             
-    def evaluate(self, data_loader):
-        """
-        To fix
-        """
-        correct = 0
-        total = 0
-        confusion_mat=[[0 for j in range(self.net.output_classes)]for i in range(self.net.output_classes)]
-        for data in data_loader:
+        self.net = cudaify(ProCNN(config, output_classes, char_vocab))
+        self.optimizer = torch.optim.Adam(self.net.parameters(), lr=config["learning_rate"])
+        
+    @classmethod
+    def objective(cls):
+        return torch.nn.MSELoss()
+    
+    @classmethod
+    def correlation(self):
+        return pearsonr
+    
+    def step(self):
+        self.net.train()
+        train_loss, train_corr = self.run_one_cycle(True)
+        self.net.eval()
+        dev_loss, dev_corr = self.run_one_cycle(False)
+        
+        return {"train_loss" : train_loss,
+                "train_corr" : train_corr,
+                "dev_loss" : dev_loss,
+                "dev_corr" : dev_corr
+        }
+    
+    def run_one_cycle(self, train=True):
+        total_loss = 0
+        
+        all_labels = []
+        all_preds = []
+        
+        for data in (self.train_loader if train else self.eval_loader):
+            if train:
+                self.optimizer.zero_grad()
+        
             input_s, input_t, labels = self.tensorize(data)
-            outputs = self.net(input_s,input_t)
-            correct_inc, total_inc = CnnClassifier.accuracy(outputs, labels)
-            confusion_mat_inc = CnnClassifier.confusion(outputs, labels)
-            correct += correct_inc
-            total += total_inc
-            for i in range(len(confusion_mat)):
-                for j in range(len(confusion_mat)):
-                    confusion_mat[i][j]+=confusion_mat_inc[i][j]
-        return correct / total, confusion_mat
+            outputs = self.net(input_s, input_t).view(-1)
+            loss = self.objective()(outputs, labels)
+            
+            total_loss += loss.data.item()
+            all_labels.extend(labels.tolist())
+            all_preds.extend(outputs.tolist())
+            
+            if train:
+                loss.backward()
+                self.optimizer.step()
+                
+        total_corr = self.correlation(all_labels, all_preds)
+        return total_loss, total_corr
     
-    @staticmethod
-    def accuracy(outputs, labels):
-        correct = 0
-        total = 0
-        for output, label in zip(outputs, labels):
-            total += 1
-            if label.item() == output.argmax().item():
-                correct += 1
-        return correct, total
-    
-    @staticmethod
-    def confusion(outputs,labels):
-        mat=[[0 for j in range(len(outputs[0]))]for i in range(len(outputs[0]))]
-        for output, label in zip(outputs,labels):
-            i = output.argmax().item()
-            j=label.item()
-            mat[i][j]+=1
-        return mat
+    def save_checkpoint(self, checkpoint_dir):
+        path = os.path.join(checkpoint_dir, "checkpoint.pt")
+        
+        torch.save({
+            "epoch": self.epoch,
+            "model_state_dict": self.net.state_dict(),
+            "optimizer_state_dict": self.optimizer.state_dict()
+        }, path)
+        
+    def load_checkpoint(self, checkpoint_path):
+        state = torch.load(checkpoint_path)
+        self.net.load_state_dict(state["model_state_dict"])
+        self.optimizer.load_state_dict(state["optimizer_state_dict"])        
+            
 
-def train_network(net, tensorize, 
-                  train_loader, dev_loader, 
-                  n_epochs, learning_rate):
+def search_hyperparameters(experiment_name, search_space, output_classes, trainset, devset):
     """
-    Trains a neural network using the provided DataLoaders (for training
-    and development data).
-    
-    tensorize is a function that maps a piece of data (i.e. a dictionary)
-    to an input and output tensor for the neural network.
-    
-    """
-    print("===== HYPERPARAMETERS =====")
-    print("epochs=", n_epochs)
-    print("learning_rate=", learning_rate)
-    print("=" * 30)
-    
-    n_batches = len(train_loader)
-    print_every = n_batches // 10  
-    loss = torch.nn.CrossEntropyLoss()
-    optimizer = optim.Adam(net.parameters(), lr=learning_rate)
-    training_start_time = time.time()
-    best_accuracy = 0.0
-    best_net = None
-    for epoch in range(n_epochs):
-        running_loss = 0.0
-        start_time = time.time()
-        total_train_loss = 0
-        for i, data in enumerate(train_loader, 0):
-            input_s, input_t, labels = tensorize(data)
-            optimizer.zero_grad()
-            outputs = net(input_s,input_t)
-            #print('output: ',outputs)
-            #print('label: ',labels)
-            loss_size = loss(outputs, labels)
-            loss_size.backward()
-            optimizer.step()
-            running_loss += loss_size.data.item()
-            total_train_loss += loss_size.data.item()
-            if (i + 1) % (print_every + 1) == 0:
-                print("Epoch {}, {:d}% \t train_loss: {:.2f} took: {:.2f}s".format(
-                        epoch+1, int(100 * (i+1) / n_batches), 
-                        running_loss / print_every, time.time() - start_time))
-                #print(outputs)
-                #print(labels)
-                running_loss = 0.0
-                start_time = time.time()                    
-        #At the end of the epoch, do a pass on the validation set
-        accuracy, confusion_mat = CnnClassifier(net, tensorize).evaluate(dev_loader)
-        print(accuracy)
-        if accuracy > best_accuracy:
-            best_confusion = confusion_mat
-            print(best_confusion)
-            best_net = deepcopy(net)
-            best_accuracy=accuracy
-            torch.save(best_net,'current_best_f_cv5_lt.nnt')
-        print("Dev acc = {:.2f}".format(accuracy))
-        #print(outputs)
-        #print(labels)
-               
-    print("Training finished, took {:.2f}s".format(time.time() - training_start_time))
-    print("Best mse: ", best_mse)
-    return best_net
-
-def train_all_categories(dataset, hidden_size=512):
-    """
-    Trains a multi-way CNN classifier for all protein families in the
-    dataset.
-    
-    e.g. train_all_categories(JsonDataset('data/var7.10.json'))
-    
+    Performs a hyperparameter search of the best-performing network
+    for the given datasets (training and development).
     """   
-    def extract_amino_acid_alphabet(dataset):
-        symbols = set()
-        for sequence in dataset.select('seq'):
-            letters = set(sequence)
-            symbols = symbols | letters
-        return sorted(list(symbols))
-
-    train_sampler, val_sampler, _ = get_samplers(range(len(dataset)), 
-                                                 .25, .05)
-    char_vocab = Vocab(extract_amino_acid_alphabet(dataset))
     
-    categories = sorted(list(set(dataset.select('exp'))))
+    def extract_amino_acid_alphabet(trainset):
+        #symbols = set()
+        #for sequence in trainset.select('seq'):
+        #    letters = set(sequence)
+        #    symbols = symbols | letters
+        #return sorted(list(symbols))
+        return ['A', 'C', 'G', 'N', 'T']
+
+    char_vocab = Vocab(extract_amino_acid_alphabet(trainset))    
+    categories = sorted(list(set(trainset.select('exp'))))
     category_vocab = Vocab(categories)
-    print(category_vocab.index_map)
-    CNN = cudaify(ProCNN(len(char_vocab.alphabet),
-                            hidden_size,
-                            char_vocab, len(category_vocab)))
-    train_loader =  DataLoader(dataset, batch_size=125,
-                               sampler=train_sampler, num_workers=0)
-    dev_loader =  DataLoader(dataset, batch_size=64,
-                             sampler=val_sampler, num_workers=0)
-    bestCNN=train_network(CNN, Tensorize(char_vocab, 1024, category_vocab), 
-                  train_loader, dev_loader, n_epochs=30, learning_rate=0.0001)
-    return bestCNN
+
+    bayesopt = SkOptSearch(metric="dev_corr", mode="max")
+    
+    # Early stopper for plateauing metrics
+    stopper = ray.tune.stopper.TrialPlateauStopper("dev_corr", std=0.01, num_results=5, grace_period=5)
+    
+    # https://arxiv.org/pdf/1810.05934.pdf
+    scheduler = tune.schedulers.ASHAScheduler(
+        time_attr='training_iteration',
+        metric='dev_corr',
+        mode='max',
+        max_t=200,
+        grace_period=10,
+        reduction_factor=3,
+        brackets=1
+    )
+
+    results = tune.run(tune.with_parameters(Trainable, 
+                                            char_vocab=char_vocab,
+                                            category_vocab=category_vocab,
+                                            output_classes=output_classes,
+                                            trainset=trainset,
+                                            devset=devset),
+                        config=search_space, 
+                        name=experiment_name, 
+                        resources_per_trial={"cpu" : 40, "gpu" : 1},
+                        num_samples=1, 
+                        search_alg=bayesopt, 
+                        scheduler=scheduler,
+                        stop=stopper,
+                        fail_fast=True)
+    
+    return results
 
 
 if __name__ == "__main__":
-   train_all_categories(JsonDataset('test_file_oav_filt05_filtcv3_Zlog_tri.json'))
+    search_space = {
+       "learning_rate": tune.loguniform(1e-5, 1e-1),
+       "xt_hidden": tune.choice([16, 32, 64, 128, 256, 512, 1024]),
+       "seq_length": tune.choice([i * 100 for i in range(1, 11)]),
+       "batch_size": tune.choice([2, 4, 8, 16, 32, 64, 128, 256]),
+       "activation" : tune.choice(["relu", "gelu", "elu"]),
+    }
+
+
+    trainset = JsonDataset('/home/simonl2/yeast/Gene_Expression_Pred/October_Runs/Data/file_oav_filt05_filtcv3_Zlog_new_testGenes.json')
+    devset = JsonDataset('/home/simonl2/yeast/Gene_Expression_Pred/October_Runs/Data/file_oav_filt05_filtcv3_Zlog_new_validGenes.json')
+
+    results = search_hyperparameters("2021-11-06", search_space, 1, trainset, devset)
+    
+    best_trial = result.best_trial
+    print("Best trial config: {}".format(best_trial.config))
+    print("Best trial final validation loss: {}".format(
+        best_trial.last_result["val_loss"]))
+    print("Best trial final validation correlation: {}".format(
+        best_trial.last_result["val_corr"]))
+    print(results.results_df)
 
